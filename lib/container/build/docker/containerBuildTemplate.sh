9e9ad2fd63a21be14e99e64ced8d547572a27cbe
Ubuntu <ubuntu@ip-10-75-23-138.eu-west-1.compute.internal>
2014-06-11 21:09:51 +0000

    testing build and deploy

--- lib/container/build/docker/containerBuildTemplate.sh
+++ lib/container/build/docker/containerBuildTemplate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 sudo docker build -t __NAMESPACE__/__TARGETNAME__-__BUILDNUMBER__ .
-TMPID=$(sudo docker run -d __NAMESPACE__/__TARGETNAME__-__BUILDNUMBER__ /bin/bash)
-sudo docker export $TMPID > __BUILDPATH__/__TARGETNAME__-__BUILDNUMBER__
 
