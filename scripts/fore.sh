c5eeb2d254f13d2166c926082f902ab7cb14f409
Ubuntu <ubuntu@ip-10-75-23-138.eu-west-1.compute.internal>
2014-06-18 08:59:16 +0000

    demo cleanup

--- scripts/fore.sh
+++ scripts/fore.sh
@@ -9,8 +9,11 @@ nohup forever main.js > ../../log/nfd-kernel.log 2>&1 &
 cd ../../nfd-api
 nohup forever server.js > ../log/nfd-api.log 2>&1 &
 
-cd ../viking/web
-nohup forever index.js > ../../log/web.log 2>&1 &
+#cd ../viking/web
+#nohup forever index.js > ../../log/web.log 2>&1 &
+
+cd ../nfd-web
+nohup forever server.js > ../log/web.log 2>&1 &
 
 ps aux | grep -i node
 tail -f ../../log/*
