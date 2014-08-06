be077998917209e4a95752a049dda24e0ecc445d
Matteo Collina <matteo.collina@gmail.com>
2014-08-06 12:56:25 +0200

    Fixed the gulp coverage task.

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,32 @@ var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+
+var tests = ['test/unit/containerTest.js',
+             'test/unit/semverTest.js',
+             'test/unit/sysrevTest.js',
+             'test/unit/topologyTest.js']
 
 gulp.task('default', function() {
-    return gulp.src(['test/unit/containerTest.js', 
-                     'test/unit/semverTest.js',
-                     'test/unit/sysrevTest.js',
-                     'test/unit/topologyTest.js']).pipe(mocha({ reporter: 'list' }))
+    return gulp.src(tests).pipe(mocha({ reporter: 'list' }))
                                                   .on('error',gutil.log);
 });
 
+gulp.task('jshint', function() {
+  return gulp.src(['./lib/**/*.js', './test/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+})
 
-
-gulp.task('test', function () {
-    gulp.src(['lib/**/*.js','main.js'])
+gulp.task('coverage', function () {
+  gulp.src(['lib/**/*.js'])
     .pipe(istanbul())
-    .on('end', function () {
-         gulp.src(['test/**/*.js'])
+    .on('finish', function () {
+      gulp.src(tests)
         .pipe(mocha({
-            reporter: 'list'
+          reporter: 'list'
         }))
         .pipe(istanbul.writeReports());
-   });
+    });
 });
